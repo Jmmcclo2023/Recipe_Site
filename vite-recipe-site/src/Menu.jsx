145540a6a115fee854481ece19d36dd500a936a5
Jalen <Jmmcclo2023@gmail.com>
2023-10-07 23:22:23 -0400

    Deleted default code and added Menu component

---
+++ vite-recipe-site/src/Menu.jsx
@@ -0,0 +1,15 @@
+function Menu() {
+
+    return (
+        <>
+            <div>
+                <button>Daily Recipe</button>
+            </div>
+            <div>
+                <button>Recipe List</button>
+            </div>
+        </>
+    )
+}
+
+export default Menu
