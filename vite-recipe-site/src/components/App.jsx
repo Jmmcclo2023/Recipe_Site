7bb82898985aeb4418b08fe24f182e43b9ebe275
Jalen <Jmmcclo2023@gmail.com>
2023-10-08 00:16:06 -0400

    Restructured project folders

--- vite-recipe-site/src/components/App.jsx
+++ vite-recipe-site/src/components/App.jsx
@@ -1,10 +1,7 @@
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-
-// Cool
 import Menu from './Menu.jsx'
+import '../styles/App.css'
+
 
 function App() {
 
